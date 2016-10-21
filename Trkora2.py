16b6044a35013e2df547e86915c4858851a6a811
William Blanchett <blanchettwa@g.cofc.edu>
2016-10-21 16:18:53 -0400

    Began new Updated File

---
+++ Trkora2.py
@@ -0,0 +1,28 @@
+from midi import *
+from gui import *
+from music import *
+
+#Trikora.py: A JythonMusic project that basically works as a MIDI VST. 
+# William Blanchett. Last Updated October 21, 2016
+# Basic Program Process:
+#         Create a MIDI Device. Obtain input from MIDI device. Split input into
+#				three different channels. Make each channel have its own MIDI
+#               voice. Allow each voice to have independent volume, tuning
+#               (possibly panning). 
+#               
+# Stretch Goals: Graphic Display, Autopilot mode (Recursive Generator)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
